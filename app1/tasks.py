3faceb471e81a176325c48738c2cf2a615235369
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-29 15:33:21 -0500

    Implementing Thread

--- app1/tasks.py
+++ app1/tasks.py
@@ -1,7 +1,13 @@
 import time
+from datetime import datetime
 from threading import Thread
+
+
+def async_send_email(to):
+    task = Thread(target=send_mail, args=(to,))
+    task.start()
 
 
 def send_mail(to):
     time.sleep(2)
-    print("Sending mail")
+    print(">>> Email sent to", to, "at", datetime.now())
--- app1/views.py
+++ app1/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from app1.tasks import send_mail
+from app1.tasks import async_send_email
 
 
 def index(request):
     mail_sent = False
     if request.method == 'POST':
         email = request.POST.get('email')
-        send_mail(email)
+        async_send_email(email)
         mail_sent = True
 
     return render(request, 'index.html', {
-        'mail_sent': False
+        'mail_sent': mail_sent
     })

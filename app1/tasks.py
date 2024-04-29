bd470c75d9af320e2de04805c5d3619b9906680f
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-29 15:24:20 -0500

    Implement send email

---
+++ app1/tasks.py
@@ -0,0 +1,7 @@
+import time
+from threading import Thread
+
+
+def send_mail(to):
+    time.sleep(2)
+    print("Sending mail")
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from app1.tasks import send_mail
+
 
 def index(request):
+    mail_sent = False
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        send_mail(email)
+        mail_sent = True
+
     return render(request, 'index.html', {
         'mail_sent': False
     })

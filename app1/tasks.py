b93f48597b3e487be304dd1d10b570dc9054beaf
Miguel Canessa <canessaalvamiguel@gmail.com>
2024-04-30 00:29:24 -0500

    Using group, chain, chord method from Celery

--- app1/tasks.py
+++ app1/tasks.py
@@ -7,3 +7,20 @@ from celery import shared_task
 def send_mail(to):
     time.sleep(2)
     print(f">>> Email sent to {to} at {datetime.now()}")
+    return True
+
+
+@shared_task
+def send_welcome_email(to):
+    print(f">>> Welcome email sent to {to} at {datetime.now()}")
+    return True
+
+
+@shared_task
+def send_subscription_email(to):
+    print(f">>> Subscription email sent to {to} at {datetime.now()}")
+    return True
+
+@shared_task
+def update_mails_sent(*args):
+    print(f">>> All emails were sent")
--- app1/views.py
+++ app1/views.py
@@ -1,17 +1,47 @@
 from datetime import timedelta, datetime
 
 from django.shortcuts import render
-from app1.tasks import send_mail
+from app1.tasks import send_mail, send_welcome_email, send_subscription_email, update_mails_sent
+from celery import group, chain, chord
 
 
 def index(request):
     mail_sent = False
     if request.method == 'POST':
         email = request.POST.get('email')
-        send_mail.apply_async(
-            args=[email],
-            #countdown=10
-            eta=datetime.now() + timedelta(seconds=10)
+        # send_mail.apply_async(
+        #     args=[email],
+        #     #countdown=10
+        #     eta=datetime.now() + timedelta(seconds=10)
+        # )
+
+        # tasks = group(
+        #     send_mail.s(email),
+        #     send_welcome_email.s(email),
+        #     send_subscription_email.s(email)
+        # )
+        # tasks.apply_async(
+        #     countdown=10
+        # )
+
+        # tasks = chain(
+        #     send_mail.s(email),
+        #     send_welcome_email.s(),
+        #     send_subscription_email.s()
+        # )
+        # tasks.apply_async(
+        #          countdown=10
+        # )
+
+        # Callback runs only if all task are executed successfully
+        chord(
+            [
+                send_mail.s(email),
+                send_welcome_email.s(email),
+                send_subscription_email.s(email)
+            ]
+        )(
+            update_mails_sent.s()
         )
         mail_sent = True
 
